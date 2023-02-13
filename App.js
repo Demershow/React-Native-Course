7d3a5e538961de2f8677c5f312562aae640cb72e
demerson <demersontorres520@gmail.com>
2023-02-13 20:17:39 -0300

    Aula 13: Criando Componentes

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { View, Text, Image } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+export default class App extends Component{
+  render(){
+
+    let nome = 'Demerson'
+
+    return(
+      <View>
+        <Text>
+          olá mundo
+        </Text>
+        <Text style={{ color: '#ff0000'}}>
+          sujeito programador
+        </Text>
+        <Jobs
+        Largura={200} altura={200} fulano='Steve Jobs'
+         />
+         <Text>
+          {nome}
+         </Text>
+      </View>
+    )
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+class Jobs extends Component{
+  render(){
+    return(
+      <View>
+      <Image source={{ uri: 'https://sujeitoprogramador.com/steve.png' }} 
+        style={{width: this.props.largura, height: this.props.altura}}
+        />
+        <Text>
+          {this.props.fulano}
+        </Text>
+      </View>
+    )
+  }
+}

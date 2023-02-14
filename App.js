a1f728ad520289ef23dae990dfe93dab89c0fff0
demerson <demersontorres520@gmail.com>
2023-02-14 20:20:04 -0300

    Aula 19: Recebendo dados

--- App.js
+++ App.js
@@ -1,17 +1,60 @@
 import React, {Component} from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
 
 
 export default class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      nome: ''
+    }
+
+    this.takeName = this.takeName.bind(this)
+  }
+
+  takeName(texto) {
+    if (texto.length > 0) {
+      this.setState({nome: 'Bem vindo: ' + texto})
+    } else {
+      this.setState({nome: ''})
+    }
+  }
+
   render(){
     return(
-      <View style={{flex:1, backgroundColor: '#ddd'}}>
-        <View style={{height: 65, backgroundColor: 'gray'}}></View>
-        <View style={{flex:1, backgroundColor: '#fff'}}></View>
-        <View style={{height: 65, backgroundColor: 'gray'}}></View>
+      <View style={styles.container}>
+        <TextInput 
+          style={styles.input}
+          placeholder="Bem vindo, digite seu nome"
+          underlineColorAndroid='transparent'
+          onChangeText={this.takeName}
+        />
+        <Text style={styles.text}>
+          {this.state.nome}
+        </Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 20
+  },
+  input: {
+    height: 45,
+    borderWidth: 1,
+    borderColor: '#333',
+    margin: 10,
+    fontSize: 20,
+    padding: 10
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 25
+  }
+})
 
 

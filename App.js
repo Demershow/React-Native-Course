e797d126471a7c27722061f35de65cd329acaf14
demerson <demersontorres520@gmail.com>
2023-02-14 19:28:00 -0300

    Aula 14: entendendo os states

--- App.js
+++ App.js
@@ -1,43 +1,40 @@
 import React, {Component} from 'react';
-import { View, Text, Image } from 'react-native';
+import { View, Text, Button } from 'react-native';
 
 
 export default class App extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      nome: 'matheus'
+    };
+
+    this.entrar = this.entrar.bind(this)
+
+  }
+
+  entrar(nome) {
+    this.setState({
+      nome
+    })
+  }
+
   render(){
 
     let nome = 'Demerson'
 
     return(
-      <View>
-        <Text>
-          olá mundo
-        </Text>
-        <Text style={{ color: '#ff0000'}}>
-          sujeito programador
-        </Text>
-        <Jobs
-        Largura={200} altura={200} fulano='Steve Jobs'
-         />
-         <Text>
-          {nome}
-         </Text>
-      </View>
-    )
-  }
-}
-
-
-class Jobs extends Component{
-  render(){
-    return(
-      <View>
-      <Image source={{ uri: 'https://sujeitoprogramador.com/steve.png' }} 
-        style={{width: this.props.largura, height: this.props.altura}}
+      <View style={{marginTop: 20}}>
+        <Button 
+          title='Next'
+          onPress={() => this.entrar('Julio')}
         />
-        <Text>
-          {this.props.fulano}
+        <Text style={{fontSize: 20, textAlign: 'center'}}>
+          {this.state.nome}
         </Text>
       </View>
     )
   }
 }
+
+

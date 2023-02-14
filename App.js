5a210c37e083e92ece999f35eecfaf9a3abf9748
demerson <demersontorres520@gmail.com>
2023-02-14 19:48:08 -0300

    Aula 16: Tamanhos dinamicos e fixos

--- App.js
+++ App.js
@@ -5,25 +5,13 @@ import { View, Text, Button, StyleSheet } from 'react-native';
 export default class App extends Component{
   render(){
     return(
-      <View style={styles.container}>
-        <Text style={styles.text}>TEXTO 1</Text>
-        <Text style={styles.text}>TEXTO 2</Text>
-        <Text style={styles.text}>TEXTO 3</Text>
-        <Text style={styles.text}>TEXTO 4</Text>
+      <View style={{flex:1, backgroundColor: '#ddd'}}>
+        <View style={{height: 65, backgroundColor: 'gray'}}></View>
+        <View style={{flex:1, backgroundColor: '#fff'}}></View>
+        <View style={{height: 65, backgroundColor: 'gray'}}></View>
       </View>
     )
   }
 }
-
-const styles = StyleSheet.create({
-    container : {
-      marginTop: 20,
-    },
-    text: { 
-      textAlign: 'center',
-      fontSize: 25,
-      color: 'red'
-    }
-})
 
 

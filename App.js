18e34a4827e3ec1ea089e818ccc38ebcf4c58610
demerson <demersontorres520@gmail.com>
2023-02-14 19:37:16 -0300

    Aula 15: Aplicando grupo de estilos

--- App.js
+++ App.js
@@ -1,40 +1,29 @@
 import React, {Component} from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 
 export default class App extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      nome: 'matheus'
-    };
-
-    this.entrar = this.entrar.bind(this)
-
-  }
-
-  entrar(nome) {
-    this.setState({
-      nome
-    })
-  }
-
   render(){
-
-    let nome = 'Demerson'
-
     return(
-      <View style={{marginTop: 20}}>
-        <Button 
-          title='Next'
-          onPress={() => this.entrar('Julio')}
-        />
-        <Text style={{fontSize: 20, textAlign: 'center'}}>
-          {this.state.nome}
-        </Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>TEXTO 1</Text>
+        <Text style={styles.text}>TEXTO 2</Text>
+        <Text style={styles.text}>TEXTO 3</Text>
+        <Text style={styles.text}>TEXTO 4</Text>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    container : {
+      marginTop: 20,
+    },
+    text: { 
+      textAlign: 'center',
+      fontSize: 25,
+      color: 'red'
+    }
+})
 
 

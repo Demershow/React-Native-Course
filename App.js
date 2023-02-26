d3ef2251c80c2fd433e81b6f0247cd205141364a
demerson <demersontorres520@gmail.com>
2023-02-26 11:13:55 -0300

    Aula #30 instaSujeito

--- App.js
+++ App.js
@@ -1,100 +1,126 @@
-import React, {Component} from 'react';
-import { View, Text, TouchableOpacity , Image, StyleSheet } from 'react-native';
-import Slider from '@react-native-community/slider';
 
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
 
-export default class App extends Component{
+import Lista from './src/Lista';
+
+class App extends Component {
 
   constructor(props){
     super(props);
     this.state = {
-      textFrase: 'Frase',
-      img: require('./assets/biscoito.png'),
-      valor: 0
-    }
-
-    this.frases = [
-      'Espernça é a primeira que morre',
-      'O que está ruim, sempre pode piorar',
-      'Você nunca está sozinha, sempre tem alguem te observando',
-      'A vida é uma caixinha de surpresas',
-      'Olha eu sem boné, olha eu com boné',
-      'Com quantos paus se faz uma canoa? eu sei que o titanic foi feito com o meu'
-    ]
-    this.quebraBiscoito = this.quebraBiscoito.bind(this);
-
+      feed: [
+        {
+          id: '1', 
+          nome: 'Lucas Silva', 
+          descricao: 'Mais um dia de muitos bugs :)', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto1.png',  
+          likeada: false, 
+          likers: 0
+         },
+        {
+          id: '2', 
+          nome: 'Matheus', 
+          descricao: 'Isso sim é ser raiz!!!!!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto2.png', 
+          likeada: false, 
+          likers: 0
+        },
+        {
+          id: '3', 
+          nome: 'Jose Augusto', 
+          descricao: 'Bora trabalhar Haha', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil3.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto3.png',  
+          likeada: false, 
+          likers: 3
+        },
+        {
+          id: '4', 
+          nome: 'Gustavo Henrique', 
+          descricao: 'Isso sim que é TI!', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil1.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto4.png', 
+          likeada: false, 
+          likers: 1
+        },
+        {
+          id: '5', 
+          nome: 'Guilherme', 
+          descricao: 'Boa tarde galera do insta...', 
+          imgperfil: 'https://sujeitoprogramador.com/instareact/fotoPerfil2.png', 
+          imgPublicacao: 'https://sujeitoprogramador.com/instareact/foto5.png',
+          likeada: false, 
+          likers: 32
+        }
+      ]
+     };
+ 
   }
 
-  quebraBiscoito(){
-    let numeroAleatorio = Math.floor(Math.random() * this.frases.length)
-    this.setState({
-      textFrase: '"' + this.frases[numeroAleatorio] + '"',
-      img: require('./assets/biscoitoAberto.png')
-    })
-  }
-
-  render(){
-    return(
+  render() {
+    return (
       <View style={styles.container}>
-        <Image style={styles.img} source={this.state.img}  />
-        <Text style={styles.textoFrase}>{this.state.textFrase}</Text>
-        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
-          <View style={styles.btnArea}>
-            <Text styles={styles.btnTexto}>Quebrar biscoito</Text>
-          </View>
+      
+      <View style={styles.header}>
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/logo.png')}
+          style={styles.logo}
+          />
         </TouchableOpacity>
-        <Slider
-          style={{width: 200, height: 40}}
-          minimumValue={0}
-          maximumValue={100}
-          minimumTrackTintColor="#dd7b22"
-          onValueChange={(valor) => this.setState({valor})}
-          value={this.state.valor}
-        />
-        <Text>
-          {this.state.valor.toFixed(0)}
-        </Text>
+
+        <TouchableOpacity>
+          <Image
+          source={require('./src/img/send.png')}
+          style={styles.send}
+          />
+        </TouchableOpacity>
       </View>
-    )
+
+      <FlatList
+      showsHorizontalScrollIndicator={false}
+      data={this.state.feed}
+      renderItem={ ({item}) => <Lista data={item} /> }
+      />
+
+      </View>
+    );
   }
 }
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 20,
+  },
+  header:{
+    height: 55,
+    backgroundColor: '#FFF',
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'space-between',
+    padding: 5,
+    borderBottomWidth: 0.2,
+    shadowColor: '#000',
+    elevation: 1,
   },
-  img: {
-    width: 250,
-    height: 250
-  },
-  textoFrase: {
-    fontSize: 20,
-    color: '#dd7b22',
-    margin: 30,
-    fontStyle: 'italic',
-    textAlign: 'center'
-  },
-  botao: {
-    width: 230,
-    height: 50,
-    borderWidth: 2,
-    borderColor: '#dd7b22',
-    borderRadius: 25
-  },
-  btnArea: {
-     flex: 1,
-     flexDirection: 'row',
-     justifyContent: 'center',
-     alignItems: 'center'
-  },
-  btnTexto: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#dd7b22'
+  send:{
+    width: 23,
+    height: 23
   }
-})
 
+});
 
+export default App;
